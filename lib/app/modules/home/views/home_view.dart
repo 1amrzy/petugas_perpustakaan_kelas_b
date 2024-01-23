b1ef1c5f6c2b6ff49564b2b1de15973fd704b88e
CilokMini11 <yukiiii050911@gmail.com>
2024-01-23 14:54:35 +0700

    second commit ( Membuat button untuk navigate ke halaman berikutnya )

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:petugas_perpustakaan_kelas_b/app/routes/app_pages.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: (){
+              controller.buku();
+            },
+            child: Text("Book"),
+            ),
+            ElevatedButton(onPressed: (){
+              controller.peminjaman();
+            },
+              child: Text("Peminjaman"),
+            ),
+              ]
+        ),
+        ),
+      );
+  }
+}
